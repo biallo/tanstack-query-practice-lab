997054c58510e051211ee94f042461acc459c6bf
Biallo <bialloworld@gmail.com>
2026-05-10 21:56:35 -0400

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/tanstack-query-practice-lab/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/tanstack-query-practice-lab/',
   plugins: [react()],
-})
+}))
